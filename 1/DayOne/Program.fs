678d73f8fa45bb27cb814ba0a84ef41545b82b36
Maximilian Lösch <Maximilian.Loesch@outlook.com>
2024-12-01 23:26:44 +0100

    solution to problem one

--- 1/DayOne/Program.fs
+++ 1/DayOne/Program.fs
@@ -1,12 +1,19 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open System
 
-let read = Seq.initInfinite( fun _ -> Console.ReadLine()) 
-        |> Seq.takeWhile(fun x -> not (x = null))
+let read =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun x -> not (x = null))
 
 [<EntryPoint>]
-let main argv  =
-        let list = read
-        for s in list do
-                printf "%s\n" s
-        0
+let main argv =
+    let (left, right) =
+        read
+        |> Seq.map (fun x -> x.Split "   " |> Seq.map (int))
+        |> Seq.fold (fun state element -> (Seq.head element :: fst state, Seq.last element :: snd state)) ([], [])
+
+    (0, List.sort left, List.sort right)
+    |||> Seq.fold2 (fun acc a b -> acc + abs (a - b))
+    |> printfn "%A"
+
+    0
