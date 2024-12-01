e85fe0ac9525b2e241a08f8d6e0bcae8758688f4
Maximilian Lösch <Maximilian.Loesch@outlook.com>
2024-12-01 19:51:19 +0100

    getting a handle on parsing

---
+++ 1/DayOne/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
+let read = Seq.initInfinite( fun _ -> Console.ReadLine()) 
+        |> Seq.takeWhile(fun x -> not (x = null))
+
+[<EntryPoint>]
+let main argv  =
+        let list = read
+        for s in list do
+                printf "%s\n" s
+        0
